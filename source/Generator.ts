bd956482522df73d53dea780b6727e018e5db232
Simon Mika <simon@mika.se>
2016-07-08 19:37:42 +0200

    Added support for combining generators.

--- source/Generator.ts
+++ source/Generator.ts
@@ -1,7 +1,9 @@
 import { Task } from './Task'
+import * as Random from './Random'
 
 export abstract class Generator {
-	constructor(private name: string) {	}
+	constructor(private name: string) { }
+	getName(): string { return this.name }
 	abstract next(): Task
 	generate(count: number): Task[] {
 		var result = <Task[]>[]
@@ -14,17 +16,32 @@ export abstract class Generator {
 		this.generators.push(generator)
 	}
 	static get(name: string): Generator {
-		var result: Generator
-		this.generators.forEach(generator => {
-			if (generator.name == name)
-				result = generator
+		var result = name.split('+').map(name => {
+			var r: Generator
+			this.generators.forEach(generator => {
+				if (generator.name == name)
+					r = generator
+			})
+			return r
 		})
-		return result
+		return !result || result.length < 1 ? null :
+			result.length > 1 ? new CombinedGenerator(result) :
+			result[0]
 	}
 	static next(name: string): Task {
 		return Generator.get(name).next()
 	}
 	static generate(name: string, count: number): Task[] {
 		return Generator.get(name).generate(count)
+	}
+}
+export class CombinedGenerator extends Generator {
+	private random: Random.Generator
+	constructor(private generators: Generator[]) {
+		super(generators.map(generator => generator.getName()).join('+'));
+		this.random = new Random.Integer(new Random.Uniform(0, generators.length))
+	}
+	next(): Task {
+		return this.generators[this.random.next()].next()
 	}
 }
